31abd762a0082905924de2a8cf0329a4177647c8
Donbosco Kim <leonbosco2@gmail.com>
2023-04-22 10:18:30 -0500

    added a function get_num_managers to query the num of managers in each dept

--- mysql-app.py
+++ mysql-app.py
@@ -13,6 +13,20 @@ def get_uk_employees(mycursor):
     #loop through results and show the number of employees in UK
     for record in query_result:
         print(f"United Kingdom: {record[1]}")
+    return
 
+def get_num_managers(mycursor):
+    sql_query = "select department_name, COUNT(*) AS 'Number of Managers' from managers group by department_name"
+
+    #execute the query
+    mycursor.execute(sql_query)
+
+    #get the query result
+    query_result = mycursor.fetchall()
+    
+    #loop through results and show the number of managers in each dept
+    print("\n-----Number of Managers in Eache Dept-----\n")
+    for record in query_result:
+        print(f"{record[0]}: {record[1]} managers")
     return
 
